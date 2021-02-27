b920c8db7397a07c8408aa5018ee574687843889
wslmacieiradev <wslmacieira@outlook.com>
2021-02-27 09:24:32 -0300

    Projeto criado

---
+++ BlogPessoal/BlogPessoal.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BlogPessoal.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
